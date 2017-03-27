3bb354bd22a1db3c68d8c3850590ae611d213373
dcriger <dcriger@mail.csuchico.edu>
2017-03-27 12:07:35 -0700

    Add files via upload
    Added functions to Piezas.cpp and one test to PiezasTest.cpp

--- Piezas.cpp
+++ Piezas.cpp
@@ -21,12 +21,25 @@
  * specifies it is X's turn first
 **/
 Piezas::Piezas();
+{
+    reset();
+    turn = X;
+}
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
 **/
 void Piezas::reset();
+{
+    for(int i = 0; i < BOARD_ROWS; i++)
+    {
+        for(int j = 0; j < BOARD_COLS; j++)
+        {
+            board[i][j] = Blank;
+        }
+    }
+}
 
 /**
  * Places a piece of the current turn on the board, returns what
@@ -37,12 +50,94 @@ void Piezas::reset();
  * Trying to drop a piece where it cannot be placed loses the player's turn
 **/ 
 Piece Piezas::dropPiece(int column);
+{
+    //Out of bounds placement means turn switches, return valid
+    if(column > 3 || column < 0)
+    {
+        if(turn == X)
+        {
+            turn = O;
+        }
+        else
+        {
+            turn = X;
+        }
+
+        return Invalid;
+    }
+
+    //checking from bottom of column to the top, else return nothing
+    if(board[2][column] == Blank)
+    {
+        board[2][column] = turn;
+
+        if(turn == X)
+        {
+            turn = O;
+        }
+        else
+        {
+            turn = X;
+        }
+
+        return board[2][column];
+    }
+    else if(board[1][column] == Blank)
+    {
+        board[1][column] == turn;
+
+        if(turn == X)
+        {
+            turn = O;
+        }
+        else
+        {
+            turn = X;
+        }
+
+        return board[1][column];
+    }
+    else if(board[0][column] == Blank)
+    {
+        board[0][column] == turn;
+
+        if(turn == X)
+        {
+            turn = O;
+        }
+        else
+        {
+            turn = X;
+        }
+
+        return board[0][column];
+    }
+    else
+    {
+        return Blank;
+    }
+}
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
 **/
 Piece Piezas::pieceAt(int row, int column);
+{
+    if((row > 2 || row < 0) || (column < 0 || column > 3))
+    {
+        return Invalid;
+    }
+
+    if(board[row][column] != Blank)
+    {
+        return board[row][column];
+    }
+    else
+    {
+        return Blank;
+    }
+}
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
@@ -54,3 +149,411 @@ Piece Piezas::pieceAt(int row, int column);
  * line, it is a tie.
 **/
 Piece Piezas::gameState();
+{
+    int max_x = 0;
+    int max_o = 0;
+    int x_amount;
+    int o_amount;
+    bool full_col_x = false;
+    bool full_col_o = false;
+    bool three_adj_x = false;
+    bool three_adj_o = false;
+    bool two_adj_x = false;
+    bool two_adj_o = false;
+
+    for(int i = 0; i < BOARD_ROWS; i++)
+    {
+        for(int j = 0; j < BOARD_COLS; j++)
+        {
+            if(board[i][j] == Blank)
+            {
+                return Invalid;
+            }
+        }
+    }
+
+    //check horizontal
+    for(int i = 0; i < BOARD_ROWS; i++)
+    {
+        x_amount = 0;
+        o_amount = 0;
+
+        for(int j = 0; j < BOARD_COLS; j++)
+        {
+            if(board[i][j] == X)
+            {
+                x_amount++;
+            }
+            else
+            {
+                o_amount++;
+            }
+        }
+
+        if(x_amount > max_x)
+        {
+            max_x = x_amount;
+        }
+        if(o_amount > max_o)
+        {
+            max_o = o_amount;
+        }
+    }
+
+    //check for any max values of 4, that would automatically win or tie
+    if(max_x == BOARD_COLS && max_o == BOARD_COLS)
+    {
+        return Blank;
+    }
+    else if(max_x == BOARD_COLS)
+    {
+        return X;
+    }
+    else if(max_o == BOARD_COLS)
+    {
+        return O;
+    }
+
+    //check verticals
+    //check column 0
+    if(board[0][0] == X)
+    {
+        if(board[1][0] == X)
+        {
+            if(board][2][0] == X)
+            {
+                x_amount = 3;
+                full_col_x = true;
+            }
+            else
+            {
+                x_amount = 2;
+            }
+        }
+        else
+        {
+            x_amount = 1;
+        }
+    }
+    else
+    {
+        if(board[1][0] == O)
+        {
+            if(board[2][0] == O)
+            {
+                o_amount = 3;
+                full_col_o = true;
+            }
+            else
+            {
+                o_amount = 2;
+            }
+        }
+        else
+        {
+            o_amount = 1;
+        }
+    }
+    if(x_amount > max_x)
+    {
+        max_x = x_amount;
+    }
+    if(o_amount > max_o)
+    {
+        max_o = o_amount;
+    }
+
+    //check column 1
+    if(board[0][1] == X)
+    {
+        if(board[1][1] == X)
+        {
+            if(board][2][1] == X)
+            {
+                x_amount = 3;
+                full_col_x = true;
+            }
+            else
+            {
+                x_amount = 2;
+            }
+        }
+        else
+        {
+            x_amount = 1;
+        }
+    }
+    else
+    {
+        if(board[1][1] == O)
+        {
+            if(board[2][1] == O)
+            {
+                o_amount = 3;
+                full_col_o = true;
+            }
+            else
+            {
+                o_amount = 2;
+            }
+        }
+        else
+        {
+            o_amount = 1;
+        }
+    }
+    if(x_amount > max_x)
+    {
+        max_x = x_amount;
+    }
+    if(o_amount > max_o)
+    {
+        max_o = o_amount;
+    }
+
+    //check column 2
+    if(board[0][2] == X)
+    {
+        if(board[1][2] == X)
+        {
+            if(board][2][2] == X)
+            {
+                x_amount = 3;
+                full_col_x = true;
+            }
+            else
+            {
+                x_amount = 2;
+            }
+        }
+        else
+        {
+            x_amount = 1;
+        }
+    }
+    else
+    {
+        if(board[1][2] == O)
+        {
+            if(board[2][2] == O)
+            {
+                o_amount = 3;
+                full_col_o = true;
+            }
+            else
+            {
+                o_amount = 2;
+            }
+        }
+        else
+        {
+            o_amount = 1;
+        }
+    }
+    if(x_amount > max_x)
+    {
+        max_x = x_amount;
+    }
+    if(o_amount > max_o)
+    {
+        max_o = o_amount;
+    }
+
+    //check column 3
+    if(board[0][3] == X)
+    {
+        if(board[1][3] == X)
+        {
+            if(board][2][3] == X)
+            {
+                x_amount = 3;
+                full_col_x = true;
+            }
+            else
+            {
+                x_amount = 2;
+            }
+        }
+        else
+        {
+            x_amount = 1;
+        }
+    }
+    else
+    {
+        if(board[1][3] == O)
+        {
+            if(board[2][3] == O)
+            {
+                o_amount = 3;
+                full_col_o = true;
+            }
+            else
+            {
+                o_amount = 2;
+            }
+        }
+        else
+        {
+            o_amount = 1;
+        }
+    }
+    if(x_amount > max_x)
+    {
+        max_x = x_amount;
+    }
+    if(o_amount > max_o)
+    {
+        max_o = o_amount;
+    }
+
+    //check for 3 x's in a row
+    if(max_x == BOARD_ROWS)
+    {
+        if(full_col_x == true)
+        {
+            three_adj_x = true;
+        }
+        else
+        {
+            for(int i = 0; i < BOARD_ROWS; i++)
+            {
+                if(board[i][0] == X)
+                {
+                    if(board[i][1] == X && board[i][2] == X)
+                    {
+                        three_adj_x = true;
+                    }
+                }
+                else if(board[i][1] == X)
+                {
+                    if(board[i][2] == X && board[i][3] == X)
+                    {
+                        three_adj_x = true;
+                    }
+                }
+            }
+        }
+    }
+
+    //check for 3 o's in a row
+    if(max_o == BOARD_ROWS)
+    {
+        if(full_col_o == true)
+        {
+            three_adj_o = true;
+        }
+        else
+        {
+            for(int i = 0; i < BOARD_ROWS; i++)
+            {
+                if(board[i][0] == O)
+                {
+                    if(board[i][1] == O && board[i][2] == O)
+                    {
+                        three_adj_o = true;
+                    }
+                }
+                else if(board[i][1] == O)
+                {
+                    if(board[i][2] == O && board[i][3] == O)
+                    {
+                        three_adj_o = true;
+                    }
+                }
+            }
+        }
+    }
+
+    //check for tie of 3 in a row
+    if(three_adj_x && three_adj_o)
+    {
+        return Blank;
+    }
+    else if(three_adj_x)
+    {
+        return X;
+    }
+    else if(three_adj_o)
+    {
+        return O;
+    }
+
+    //if max is 2 for either, check if they are in a row
+    if(max_x == 2)
+    {
+        for(int i = 0; i < BOARD_ROWS; i++)
+        {
+            if(board[i][0] == X && board[i][1] == X)
+            {
+                two_adj_x = true;
+            }
+            else if(board[i][1] == X && board[i][2] == X)
+            {
+                two_adj_x = true;
+            }
+            else if(board[i][2] == X && board[i][3] == X)
+            {
+                two_adj_x = true;
+            }
+        }
+        for(int j = 0; j < BOARD_COLS; j++)
+        {
+            if(board[0][j] == X && board[1][j] == X)
+            {
+                two_adj_x = true;
+            }
+            else if(board[1][j] == X && board[2][j] == X)
+            {
+                two_adj_x = true;
+            }
+        }
+    }
+
+    if(max_o == 2)
+    {
+        for(int i = 0; i < BOARD_ROWS; i++)
+        {
+            if(board[i][0] == O && board[i][1] == O)
+            {
+                two_adj_o = true;
+            }
+            else if(board[i][1] == O && board[i][2] == O)
+            {
+                two_adj_o = true;
+            }
+            else if(board[i][2] == O && board[i][3] == O)
+            {
+                two_adj_o = true;
+            }
+        }
+        for(int j = 0; j < BOARD_COLS; j++)
+        {
+            if(board[0][j] == O && board[1][j] == O)
+            {
+                two_adj_o = true;
+            }
+            else if(board[1][j] == O && board[2][j] == O)
+            {
+                two_adj_o = true;
+            }
+        }
+    }
+
+    //check for tie of 2 in a row
+    if(two_adj_x && two_adj_o)
+    {
+        return Blank;
+    }
+    else if(two_adj_x)
+    {
+        return X;
+    }
+    else if(two_adj_o)
+    {
+        return O;
+    }
+
+    //if all above fails to return, max is 1 for both, results in tie
+    return Blank;
+}
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -7,14 +7,34 @@
  
 class PiezasTest : public ::testing::Test
 {
-	protected:
-		PiezasTest(){} //constructor runs before each test
-		virtual ~PiezasTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+    protected:
+        PiezasTest(){} //constructor runs before each test
+        virtual ~PiezasTest(){} //destructor cleans up after tests
+        virtual void SetUp(){} //sets up before each test (after constructor)
+        virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
 TEST(PiezasTest, sanityCheck)
 {
-	ASSERT_TRUE(true);
+    ASSERT_TRUE(true);
+}
+
+TEST(PiezasTest, blankBoard)
+{
+    //Piece board[3][4];
+    int count = 0;
+
+    board.reset();
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            if(board[i][j] == Blank)
+            {
+                count++;
+            }
+        }
+    }
+    ASSERT_EQ(count, 12);
 }
