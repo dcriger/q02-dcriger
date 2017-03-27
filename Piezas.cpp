86a57508b4a0156489857d2e1673c3c4df65151f
dcriger <dcriger@mail.csuchico.edu>
2017-03-27 16:51:47 -0700

    Add files via upload
    hopefully fix the constructor method

--- Piezas.cpp
+++ Piezas.cpp
@@ -22,12 +22,14 @@
 **/
 Piezas::Piezas()
 {
+    board.resize(BOARD_ROWS);
     for(int i = 0; i < BOARD_ROWS; i++)
     {
-        for(int j = 0; j < BOARD_COLS; j++)
+        board[i].resize(BOARD_COLS);
+        /*for(int j = 0; j < BOARD_COLS; j++)
         {
             board[i][j] = Blank;
-        }
+        }*/
     }
     turn = X;
 }
