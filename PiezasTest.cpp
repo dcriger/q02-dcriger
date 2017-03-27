c65c22bc5c281d771a9c297fd329a23f40718310
dcriger <dcriger@mail.csuchico.edu>
2017-03-27 12:26:51 -0700

    Add files via upload
    added a second test and fixed the first one

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -22,7 +22,7 @@ TEST(PiezasTest, sanityCheck)
 TEST(PiezasTest, blankBoard)
 {
     Piezas game;
-    //Piece board[3][4];
+    Piece board[3][4];
     int count = 0;
 
     game.reset();
@@ -38,4 +38,15 @@ TEST(PiezasTest, blankBoard)
         }
     }
     ASSERT_EQ(count, 12);
+}
+
+TEST(PiezasTest, placeFirstX)
+{
+    Piezas game;
+    Piece board[3][4];
+    Piece confirm;
+
+    confirm = game.dropPiece(2);
+
+    ASSERT_EQ(confirm, X);
 }
